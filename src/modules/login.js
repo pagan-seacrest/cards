2a5639f7dbc79cf23988bb4fa03134f570df6b79
Valery "Pagan" Shvets <paganseacrest@gmail.com>
2021-02-16 01:18:20 +0200

    all forms to post

--- src/modules/login.js
+++ src/modules/login.js
@@ -1,7 +1,16 @@
 import { root, loginOrCreate, account, config } from "./config.js";
 import Auth from "./Auth.js";
+import Client from "./Client.js";
+import ElementHandler from "./ElementHandler.js"
+
+function emptyCase() {
+    const empty = new ElementHandler ([], {element: "p", id: "no-items", parentElement: root});
+    empty.addElement()
+    config.element("no-items").textContent = "Жодної картки візиту не було додано";
+}
 
 const auth = new Auth({position: root, formID: "auth"});
+    const client = new Client();
 
 export default async function login() {
     
@@ -21,4 +30,8 @@ export default async function login() {
     
     loginOrCreate.removeEventListener("click", login);
     config.token() !== null ? config.loginToggle(true) : false;
+    client.get().then(res => {
+        res.length === 0 ? emptyCase() : false;
+    });
+            
 }
