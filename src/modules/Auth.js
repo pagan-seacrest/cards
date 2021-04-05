f68685eb938232347fbfac1991a0166b5ac329c5
pagan <pagansxc@vivaldi.net>
2021-04-05 22:21:15 +0300

    bottle

--- src/modules/Auth.js
+++ src/modules/Auth.js
@@ -3,11 +3,11 @@ import {button, config } from "./config.js";
 
 export default class Auth {
   constructor() {
-    config.token() ?  button.textContent = "Створити візит" : false; 
+    config.token() ?  button.textContent = "Створити візит" : false;
   }
 
-  // sighIn () {
-  //   client.login().then(res => localStorage.setItem("token", res));
-  //   changeButtonsValue();
-  // }
+  sighIn () {
+    client.login().then(res => localStorage.setItem("token", res));
+    changeButtonsValue();
+  }
 }
